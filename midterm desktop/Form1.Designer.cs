e4eeb6c1f79ca3f009eaa9b0530b21858e3350e9
Nguyễn Mai Anh <maianh.ocm@gmail.com>
2025-05-16 15:17:01 +0700

    Update Form1.Designer.cs

--- midterm desktop/Form1.Designer.cs
+++ midterm desktop/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace midterm_desktop
+namespace Bai1
 {
     partial class Form1
     {
@@ -28,21 +28,228 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.grpEmployeeDetails = new System.Windows.Forms.GroupBox();
+            this.dtpNgaySinh = new System.Windows.Forms.DateTimePicker();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.lstThongTinNhanVien = new System.Windows.Forms.ListView();
+            this.MaSo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.HoTen = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.NgaySinh = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Email = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.txtMaSo = new System.Windows.Forms.TextBox();
+            this.lblMaSo = new System.Windows.Forms.Label();
+            this.lblHoTen = new System.Windows.Forms.Label();
+            this.lblNgaySinh = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.grpEmployeeDetails.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // grpEmployeeDetails
+            // 
+            this.grpEmployeeDetails.Controls.Add(this.lblEmail);
+            this.grpEmployeeDetails.Controls.Add(this.dtpNgaySinh);
+            this.grpEmployeeDetails.Controls.Add(this.lblNgaySinh);
+            this.grpEmployeeDetails.Controls.Add(this.btnThem);
+            this.grpEmployeeDetails.Controls.Add(this.lblHoTen);
+            this.grpEmployeeDetails.Controls.Add(this.btnLuu);
+            this.grpEmployeeDetails.Controls.Add(this.lblMaSo);
+            this.grpEmployeeDetails.Controls.Add(this.btnThoat);
+            this.grpEmployeeDetails.Controls.Add(this.lstThongTinNhanVien);
+            this.grpEmployeeDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpEmployeeDetails.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.grpEmployeeDetails.Location = new System.Drawing.Point(2, 4);
+            this.grpEmployeeDetails.Name = "grpEmployeeDetails";
+            this.grpEmployeeDetails.Size = new System.Drawing.Size(797, 444);
+            this.grpEmployeeDetails.TabIndex = 0;
+            this.grpEmployeeDetails.TabStop = false;
+            this.grpEmployeeDetails.Text = "Thông tin nhân viên";
+            // 
+            // dtpNgaySinh
+            // 
+            this.dtpNgaySinh.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpNgaySinh.Location = new System.Drawing.Point(125, 99);
+            this.dtpNgaySinh.Name = "dtpNgaySinh";
+            this.dtpNgaySinh.Size = new System.Drawing.Size(641, 22);
+            this.dtpNgaySinh.TabIndex = 12;
+            // 
+            // btnThem
+            // 
+            this.btnThem.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnThem.Location = new System.Drawing.Point(265, 188);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 32);
+            this.btnThem.TabIndex = 8;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = false;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnLuu.Location = new System.Drawing.Point(346, 188);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(75, 32);
+            this.btnLuu.TabIndex = 9;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = false;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnThoat.Location = new System.Drawing.Point(427, 188);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 32);
+            this.btnThoat.TabIndex = 10;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = false;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // lstThongTinNhanVien
+            // 
+            this.lstThongTinNhanVien.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.MaSo,
+            this.HoTen,
+            this.NgaySinh,
+            this.Email});
+            this.lstThongTinNhanVien.FullRowSelect = true;
+            this.lstThongTinNhanVien.GridLines = true;
+            this.lstThongTinNhanVien.HideSelection = false;
+            this.lstThongTinNhanVien.Location = new System.Drawing.Point(6, 249);
+            this.lstThongTinNhanVien.Name = "lstThongTinNhanVien";
+            this.lstThongTinNhanVien.Size = new System.Drawing.Size(776, 185);
+            this.lstThongTinNhanVien.TabIndex = 0;
+            this.lstThongTinNhanVien.UseCompatibleStateImageBehavior = false;
+            this.lstThongTinNhanVien.View = System.Windows.Forms.View.Details;
+            this.lstThongTinNhanVien.SelectedIndexChanged += new System.EventHandler(this.lvNhanVien_SelectedIndexChanged);
+            // 
+            // MaSo
+            // 
+            this.MaSo.Text = "Mã số";
+            this.MaSo.Width = 100;
+            // 
+            // HoTen
+            // 
+            this.HoTen.Text = "Họ Tên";
+            this.HoTen.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.HoTen.Width = 150;
+            // 
+            // NgaySinh
+            // 
+            this.NgaySinh.Text = "Ngày Sinh";
+            this.NgaySinh.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.NgaySinh.Width = 100;
+            // 
+            // Email
+            // 
+            this.Email.Text = "Email";
+            this.Email.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Email.Width = 200;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(127, 151);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(641, 20);
+            this.txtEmail.TabIndex = 0;
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(127, 61);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(641, 20);
+            this.txtHoTen.TabIndex = 2;
+            // 
+            // txtMaSo
+            // 
+            this.txtMaSo.Location = new System.Drawing.Point(127, 25);
+            this.txtMaSo.Name = "txtMaSo";
+            this.txtMaSo.Size = new System.Drawing.Size(641, 20);
+            this.txtMaSo.TabIndex = 3;
+            // 
+            // lblMaSo
+            // 
+            this.lblMaSo.AutoSize = true;
+            this.lblMaSo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaSo.Location = new System.Drawing.Point(28, 26);
+            this.lblMaSo.Name = "lblMaSo";
+            this.lblMaSo.Size = new System.Drawing.Size(46, 15);
+            this.lblMaSo.TabIndex = 4;
+            this.lblMaSo.Text = "Mã số";
+            // 
+            // lblHoTen
+            // 
+            this.lblHoTen.AutoSize = true;
+            this.lblHoTen.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoTen.Location = new System.Drawing.Point(25, 62);
+            this.lblHoTen.Name = "lblHoTen";
+            this.lblHoTen.Size = new System.Drawing.Size(49, 15);
+            this.lblHoTen.TabIndex = 5;
+            this.lblHoTen.Text = "Họ tên";
+            // 
+            // lblNgaySinh
+            // 
+            this.lblNgaySinh.AutoSize = true;
+            this.lblNgaySinh.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNgaySinh.Location = new System.Drawing.Point(25, 104);
+            this.lblNgaySinh.Name = "lblNgaySinh";
+            this.lblNgaySinh.Size = new System.Drawing.Size(70, 15);
+            this.lblNgaySinh.TabIndex = 6;
+            this.lblNgaySinh.Text = "Ngày sinh";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(25, 148);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(44, 15);
+            this.lblEmail.TabIndex = 7;
+            this.lblEmail.Text = "Email";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtHoTen);
+            this.Controls.Add(this.txtMaSo);
+            this.Controls.Add(this.grpEmployeeDetails);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Nhóm 1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpEmployeeDetails.ResumeLayout(false);
+            this.grpEmployeeDetails.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox grpEmployeeDetails;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.TextBox txtMaSo;
+        private System.Windows.Forms.Label lblMaSo;
+        private System.Windows.Forms.Label lblHoTen;
+        private System.Windows.Forms.Label lblNgaySinh;
+        private System.Windows.Forms.ListView lstThongTinNhanVien;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.ColumnHeader MaSo;
+        private System.Windows.Forms.ColumnHeader HoTen;
+        private System.Windows.Forms.ColumnHeader NgaySinh;
+        private System.Windows.Forms.ColumnHeader Email;
+        private System.Windows.Forms.DateTimePicker dtpNgaySinh;
     }
 }
 
